4b24db47339b593f36ec1d3f4204ac6332bd6b6c
Admin <deka@gmx.de>
2017-12-11 19:55:23 +0100

    Added about and preference screen

---
+++ app/src/main/java/de/udacity/dk/cleverdroid/ui/AboutActivity.java
@@ -0,0 +1,27 @@
+package de.udacity.dk.cleverdroid.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.udacity.dk.cleverdroid.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/de/udacity/dk/cleverdroid/ui/MainActivity.java
+++ app/src/main/java/de/udacity/dk/cleverdroid/ui/MainActivity.java
@@ -1,16 +1,31 @@
 package de.udacity.dk.cleverdroid.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import de.udacity.dk.cleverdroid.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         if (findViewById(R.id.fragment_container) != null) {
 
@@ -18,11 +33,31 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-            // Create a new Fragment to be placed in the activity layout
             MainFragment mainFragment = new MainFragment();
-
-            // Add the fragment to the 'fragment_container' FrameLayout
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mainFragment).commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.user_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent prefsIntent = new Intent(this, PrefsActivity.class);
+                startActivity(prefsIntent);
+                return true;
+            case R.id.action_about:
+                Intent aboutIntent = new Intent(this, AboutActivity.class);
+                startActivity(aboutIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
