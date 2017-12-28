6db5c909b9beb35f2d4449c94cfcc31e199b701b
Admin <deka@gmx.de>
2017-12-28 18:03:11 +0100

    Added possibility to repeat wrong answered and favorite questions after a session

--- app/src/main/java/de/udacity/dk/cleverdroid/ui/ResultFragment.java
+++ app/src/main/java/de/udacity/dk/cleverdroid/ui/ResultFragment.java
@@ -1,7 +1,10 @@
 package de.udacity.dk.cleverdroid.ui;
 
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -22,7 +26,13 @@ public class ResultFragment extends Fragment {
     @BindView(R.id.tv_score)
     TextView score;
 
+    @BindView(R.id.bt_again)
+    Button again;
+
     private String uriString;
+    private boolean wrongQuestionsAvailable;
+    private boolean favoriteQuestionsAvailable;
+    private Intent intent;
 
     public ResultFragment() {
     }
@@ -45,6 +55,36 @@ public class ResultFragment extends Fragment {
             score.setText(userScore + "/" + questionsAmount);
         }
 
+        if (uriString.equals(QuestionContract.URI_QUESTIONS_WRONG.toString())) {
+            FetchWrongQuestionsTask fetchWrongQuestionsTask = new FetchWrongQuestionsTask(new AsyncResponse() {
+
+                @Override
+                public void processFinish(Integer output) {
+                    if (output != 0) {
+                        wrongQuestionsAvailable = true;
+                    } else {
+                        again.setVisibility(View.INVISIBLE);
+                    }
+
+                }
+            });
+            fetchWrongQuestionsTask.execute();
+        } else if (uriString.equals(QuestionContract.URI_QUESTIONS_FAVORITE.toString())) {
+            FetchFavoriteQuestionsTask fetchFavoriteQuestionsTask = new FetchFavoriteQuestionsTask(new AsyncResponse() {
+
+                @Override
+                public void processFinish(Integer output) {
+                    if (output != 0) {
+                        favoriteQuestionsAvailable = true;
+                    } else {
+                        again.setVisibility(View.INVISIBLE);
+                    }
+
+                }
+            });
+            fetchFavoriteQuestionsTask.execute();
+        }
+
         return view;
     }
 
@@ -56,10 +96,84 @@ public class ResultFragment extends Fragment {
 
     @OnClick(R.id.bt_again)
     void repeateQuestions() {
-        Intent intent = new Intent(getActivity(), QuizActivity.class);
-        intent.putExtra(getString(R.string.key_usecase), QuestionContract.URI_QUESTIONS.toString());
+        intent = new Intent(getActivity(), QuizActivity.class);
+        if (uriString.equals(QuestionContract.URI_QUESTIONS.toString())) {
+            intent.putExtra(getString(R.string.key_usecase), QuestionContract.URI_QUESTIONS.toString());
+        } else if (uriString.equals(QuestionContract.URI_QUESTIONS_WRONG.toString())) {
+            if (wrongQuestionsAvailable) {
+                intent.putExtra(getString(R.string.key_usecase), QuestionContract.URI_QUESTIONS_WRONG.toString());
+            }
+        } else if (uriString.equals(QuestionContract.URI_QUESTIONS_FAVORITE.toString())) {
+            if (favoriteQuestionsAvailable) {
+                intent.putExtra(getString(R.string.key_usecase), QuestionContract.URI_QUESTIONS_FAVORITE.toString());
+            }
+        }
+        getActivity().finish();
         startActivity(intent);
     }
 
+    private int checkForWrongQuestions() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        Cursor cursor = resolver.query(QuestionContract.URI_QUESTIONS_WRONG,
+                null, null,
+                null, null);
+        cursor.close();
+        return cursor.getCount();
+    }
+
+    private int checkForFavoriteQuestions() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        Cursor cursor = resolver.query(QuestionContract.URI_QUESTIONS_FAVORITE,
+                null, null,
+                null, null);
+        cursor.close();
+        return cursor.getCount();
+    }
+
+    public interface AsyncResponse {
+        void processFinish(Integer output);
+    }
+
+    public class FetchWrongQuestionsTask extends AsyncTask<Void, Void, Integer> {
+
+        public AsyncResponse delegate = null;//Call back interface
+
+        public FetchWrongQuestionsTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;//Assigning call back interfacethrough constructor
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            return checkForWrongQuestions();
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            delegate.processFinish(result);
+        }
+    }
+
+    public class FetchFavoriteQuestionsTask extends AsyncTask<Void, Void, Integer> {
+
+        public AsyncResponse delegate = null;//Call back interface
+
+        public FetchFavoriteQuestionsTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;//Assigning call back interfacethrough constructor
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            return checkForFavoriteQuestions();
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            delegate.processFinish(result);
+        }
+    }
 
 }
